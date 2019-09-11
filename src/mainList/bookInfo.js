4561a9b53026c9d9fc32a75b4019c3c7ddd7b63f
Sheila Jagla <Starbone@gmail.com>
2019-09-11 12:49:14 -0600

    api returns

--- src/mainList/bookInfo.js
+++ src/mainList/bookInfo.js
@@ -12,16 +12,32 @@ import './bookInfo.css';
 
 
 class BookInfo extends Component {
-    
+
+resultsItems(){
+
+    this.props.state.searchResults.items.map((volume) => {
+      volume = {
+            title: volume.volumeInfo.title,
+            author: volume.volumeInfo.authors,
+            description: volume.volumeInfo.description,
+            publisher: volume.volumeInfo.publisher,
+            publishedDate: volume.volumeInfo.publishedDate,
+            imageLinks: volume.volumeInfo.imageLinks,
+            }})
+    return volume
+        };
+
+
+
     render() {
         // const searchResults= this.props.searchResults
 
         return (
             <section className= 'bookInfo' id= 'array spot - author last name'>
                 <div>
-                    <div><h2>Title</h2></div>
-                    <div><h3>Author: </h3> </div>
-                    <div><p>Summary: </p> </div>
+                    <div><h2>{this.volume.title}</h2></div>
+                    <div><h3>{this.volume.author}</h3> </div>
+                    <div><p>{this.volume.description}</p> </div>
                     {/* <button onClick= change the value below to true or false>+</button> */}
                 </div>
                 <div className= 'expandedBookInfo' value="false">
